22485124ada7702901ac8dd10486a944735724eb
Ronny Ac[acio <ronnyacacio27@gmail.com>
2019-11-09 10:25:35 -0300

    Interfaces completas

---
+++ Biblioteca/src/br/com/ufc/interfece/AlunoI.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ufc.interfece;
+
+import br.com.ufc.model.Aluno;
+import java.util.List;
+
+/**
+ *
+ * @author ronny
+ */
+public interface AlunoI {
+    public void cadastrarAluno(Aluno aluno);
+    public void removerAluno(int matricula);
+    public List<Aluno> alunosComLivrosEmprestados();
+    public Aluno buscarAlunoPorMatricula(int matricula);
+}
